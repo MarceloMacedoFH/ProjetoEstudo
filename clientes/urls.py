49ffbe3a8b2b86a1081a81c7d08e831265e39105
MarceloMacedoFH <marcelomf2702@hotmail.com>
2026-06-05 11:19:02 -0300

    Editar e excluir clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('clientes/', views.lista_clientes, name='lista_clientes'),
     path('clientes/criar/', views.criar_cliente, name='criar_cliente'),
+    path('clientes/editar/<int:pk>/', views.editar_cliente, name='editar_cliente'),
+    path('clientes/excluir/<int:pk>/', views.excluir_cliente, name='excluir_cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -17,3 +17,21 @@ def criar_cliente(request):
     else:
         form = ClienteForm()
     return render(request, 'clientes/criar_cliente.html', {'form': form})
+
+def editar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_clientes')
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'clientes/editar_cliente.html', {'form': form, 'cliente': cliente})
+
+def excluir_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('lista_clientes')
+    return render(request, 'clientes/confirmar_exclusao.html', {'cliente': cliente})
